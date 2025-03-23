af66340a70dd4fcaccd77a33997646b005907747
SV <guru.siva@hotmail.com>
2025-03-23 23:01:56 +0000

    Included Webkit(Safari) Browser in the config

--- playwright.config.ts
+++ playwright.config.ts
@@ -63,10 +63,10 @@ export default defineConfig({
     //   use: { ...devices['Desktop Firefox'] },
     // },
 
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
 
     /* Test against mobile viewports. */
     // {
